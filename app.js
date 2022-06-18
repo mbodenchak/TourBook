d8561bd412005fc90f3ba04d4d7444d6c30dc4e4
mbodenchak <mbodenchak@outlook.com>
2022-06-18 16:22:32 -0700

    Initial setup, data and express server

---
+++ app.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post as well!');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+const port = 3000;
+app.listen(3000, () => {
+  console.log('listening on port ' + port);
+});
