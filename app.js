a53f663a4797f0a0739ee41e99ed22b682c49b89
mbodenchak <mbodenchak@outlook.com>
2022-06-18 16:30:34 -0700

    setup post request

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello', app: 'Natours' });
 // });
@@ -14,13 +15,20 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//ROUTES
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: {
       tours: tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
 });
 
 const port = 3000;
