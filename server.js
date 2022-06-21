550c842175f0ea56b2c1364e46fcfae969ca11ba
mbodenchak <mbodenchak@outlook.com>
2022-06-21 10:17:34 -0700

    created mongoose schema and model

--- server.js
+++ server.js
@@ -17,6 +17,21 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 //START SERVER
 const port = process.env.PORT || 3000;
 app.listen(3000, () => {
