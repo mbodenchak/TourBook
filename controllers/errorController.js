d2e82aa58cc7144dae4cf78c58463e500e892a8c
mbodenchak <mbodenchak@outlook.com>
2022-06-22 13:39:53 -0700

    began distinguish prod and dev error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,38 @@
+const devError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const productionError = (err, res) => {
+  //operation, trusted error: send msg to client
+  if (err.isOperation) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //Programming or other error: don't leak details
+  } else {
+    //log error
+    console.error('*********** ERROR ***********', err);
+    //send generic msg
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went wrong.',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    devError(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    productionError(err, res);
+  }
 };
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 ///////////////////////ROUTE HANDLERS//////////////////////
 ////ALIAS ROUTE (top 5 tours)
@@ -35,6 +36,11 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 /////GET TOUR BY ID
 exports.getTourById = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -59,6 +65,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -69,7 +78,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 ////////////DELETE TOUR
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdandDelete(req.params.id);
+  const tour = await Tour.findByIdandDelete(req.params.id);
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(204).json({
     status: 'success',
     data: null,
