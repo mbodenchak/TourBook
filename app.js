5f990fb8f1fb66c38db5e16db0c6fabd2e696d94
mbodenchak <mbodenchak@outlook.com>
2022-06-23 13:15:37 -0700

    rate limiting, cookies, http headers

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
+//Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+
+app.use('/api', limiter);
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-//logs type of request and time sent .e.g: GET /api/v1/tours?duration=5&difficulty=easy 200 111.466 ms - 9387
+//Test middleware - logs type of request and time sent .e.g: GET /api/v1/tours?duration=5&difficulty=easy 200 111.466 ms - 9387
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
